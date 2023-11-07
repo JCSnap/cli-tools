fc6a17de3433c6242d5d311fd9fd0bfccb4240f6
JCSnap <e0970167@u.nus.edu>
2023-11-08 01:49:00 +0800

    made chat dynamic by taking inputs from command line

--- src/bin/chat.rs
+++ src/bin/chat.rs
@@ -5,22 +5,36 @@ use openai_api_rs::v1::chat_completion::{self, ChatCompletionRequest};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::init();
     // Make sure that you have a .env file in the root directory of the project, with your OPENAI_API_KEY
     dotenv().ok();
     let api_key = env::var("OPENAI_API_KEY").expect("OPENAI_API_KEY not found in .env file");
     let client = Client::new(api_key);
 
+    log::info!("Initialized client");
+
+    // Command line arguments
+    let args: Vec<String> = env::args().collect();
+
+    // Skip the first argument, which is the name of the program
+    let message = args[1..].join(" ");
+
+    log::info!("Sending message: {}", message);
+
     let req = ChatCompletionRequest::new(
         chat_completion::GPT4.to_string(),
         vec![chat_completion::ChatCompletionMessage {
             role: chat_completion::MessageRole::user,
-            content: String::from("Hello OpenAI!"),
+            content: message,
             name: None,
             function_call: None,
         }],
         );
     let result = client.chat_completion(req)?;
-    println!("{:?}", result.choices[0].message.content);
+
+    log::info!("Received response: {:?}", result);
+
+    println!("{}", result.choices[0].message.content.unwrap_or_default());
     Ok(())
 }
 
